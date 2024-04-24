8e77b7b3949ff2f4df9d0e3b0bc8790397bbe921
LihuelZerega <zeregalihuel@gmail.com>
2024-04-24 11:22:37 -0300

    Clients mockup

--- src/app/inicio/subcomponents/Features.tsx
+++ src/app/inicio/subcomponents/Features.tsx
@@ -8,6 +8,8 @@ import Servicios2 from "@/images/Features/Servicios2.png";
 import Servicios3 from "@/images/Features/Servicios3.png";
 import Servicios4 from "@/images/Features/Servicios4.png";
 import Servicios5 from "@/images/Features/Servicios5.png";
+import WosMockup from "@/images/Features/WosMockup.png"
+import ClientesMockup from "@/images/Features/ClientesMockup.png"
 
 const images = [
   { id: 1, src: Servicios1, alt: "Servicios1" },
@@ -59,22 +61,7 @@ function Features() {
 
         <section className="flex flex-col-reverse md:flex-row-reverse items-start justify-start mt-12 lg:mt-24">
           <div className="w-full md:w-1/2 mt-6 md:mt-0">
-            <Carousel options={{ loop: true }} plugins={[autoPlay()]}>
-              <Carousel.Slides>
-                {images.map((image) => (
-                  <Carousel.Item key={image.id}>
-                    <div className="flex items-center justify-center">
-                      <Image
-                        src={image.src}
-                        alt={image.alt}
-                        width={400}
-                        height={300}
-                      />
-                    </div>
-                  </Carousel.Item>
-                ))}
-              </Carousel.Slides>
-            </Carousel>
+            <Image src={ClientesMockup} alt="Servicios5" width={400} height={300} />
           </div>
           <div className="flex flex-col w-full md:w-1/2 lg:max-w-md md:pl-4 lg:p-4">
             <h1 className="text-lg text-center sm:text-left font-semibold text-[#720D1C]">
