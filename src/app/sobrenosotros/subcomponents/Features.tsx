3061958a8aef6dd50cc705a787c9f89bac84a08b
LihuelZerega <zeregalihuel@gmail.com>
2024-04-27 16:03:56 -0300

    last fix

--- src/app/sobrenosotros/subcomponents/Features.tsx
+++ src/app/sobrenosotros/subcomponents/Features.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Sn1 from "@/images/Features/Sn1.png";
 import Sn2 from "@/images/Features/Sn2.png";
 import Sn3 from "@/images/Backgrounds/HeroSectionBackground11.png";
+import Sn4 from "@/images/Features/Sn4.png";
 
 function Features() {
   const [ref1, inView1] = useInView({ threshold: 0.2 });
@@ -21,53 +22,51 @@ function Features() {
   }, [controls1, inView1]);
 
   return (
-    <motion.div
-      ref={ref1}
-      animate={controls1}
-      initial={{ opacity: 0, y: 20 }}
-      transition={{ duration: 0.4 }}
-      className="py-12"
-    >
-      <div className="max-w-6xl mx-auto p-6 sm:px-6 lg:px-8 pb-12">
-        <div className="flex flex-col text-left">
-          <div className="w-full">
-            <h1 className="text-2xl lg:text-3xl text-center md:text-left font-bold text-gray-800 mb-4">
-              Hace más de 20 años que trabajamos en el mercado publicitario.
-            </h1>
-            <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
-              Somos una agencia boutique que pauta gran parte del
-              entretenimiento y del espectáculo que se genera en nuestro país.
-            </p>
-            <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
-              Estas acciones nos han permitido establecer contactos fluidos con
-              los diferentes proveedores de medios, cuyo resultado nos
-              posibilita traccionar fuertemente sobre los costos.
-            </p>
-            <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
-              Podríamos haber explicitado en esta presentación, frases como
-              «servicio personalizado», «estrategias que garanticen crecimiento
-              y visibilidad » o «calidad y talento humano», pero son sólo
-              expresiones de deseo, que se repiten en forma continua en todas
-              las aperturas de página.{" "}
-            </p>
-            <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
-              Por lo tanto, sobre nosotros, únicamente vamos a manifestar que en
-              un mercado publicitario muy competitivo.... somos competitivos.
-            </p>
-          </div>
-          <div className="flex flex-col w-full items-center justify-center mt-6">
-            <div>
-              <Image src={Sn3} alt="" className="w-full lg:w-full rounded-md shadow-2xl" />
+    <>
+      <motion.div
+        ref={ref1}
+        animate={controls1}
+        initial={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.4 }}
+        className="pt-12"
+      >
+        <div className="max-w-6xl mx-auto p-6 sm:px-6 lg:px-8">
+          <div className="flex flex-row text-left">
+            <div className="wfull lg:w-1/2">
+              <h1 className="text-2xl lg:text-3xl text-center md:text-left font-bold text-gray-800 mb-4">
+                Hace más de 20 años que trabajamos en el mercado publicitario.
+              </h1>
+              <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
+                Somos una agencia boutique que pauta gran parte del
+                entretenimiento y del espectáculo que se genera en nuestro país.
+              </p>
+              <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
+                Estas acciones nos han permitido establecer contactos fluidos
+                con los diferentes proveedores de medios, cuyo resultado nos
+                posibilita traccionar fuertemente sobre los costos.
+              </p>
+              <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
+                Podríamos haber explicitado en esta presentación, frases como
+                «servicio personalizado», «estrategias que garanticen
+                crecimiento y visibilidad » o «calidad y talento humano», pero
+                son sólo expresiones de deseo, que se repiten en forma continua
+                en todas las aperturas de página.{" "}
+              </p>
+              <p className="text-lg lg:text-xl font-medium mt-3 text-neutral-600 lg:leading-8">
+                Por lo tanto, sobre nosotros, únicamente vamos a manifestar que
+                en un mercado publicitario muy competitivo.... somos
+                competitivos.
+              </p>
             </div>
-            {/* <div>
-              <Image src={Sn2} alt="" className="w-full lg:w-full rounded-md shadow-2xl my-6 md:my-16" />
-            </div> */}
-            {/* <div>
-              <Image src={Sn3} alt="" className="rounded-md shadow-2xl" />
-            </div> */}
+            <div className="hidden lg:flex flex-col w-1/2 items-start justify-start px-12">
+              <Image
+                src={Sn4}
+                alt="Sn4"
+                className="rounded-md shadow-2xl mx-12"
+              />
+            </div>
           </div>
-        </div>
-        {/* <div className="flex flex-col md:flex-row items-start">
+          {/* <div className="flex flex-col md:flex-row items-start">
           <div className="w-full md:w-1/2 px-0 md:px-4 mb-4 md:mb-0">
             <h1 className="text-center md:text-left text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-900">
               Quiénes Somos
@@ -115,8 +114,12 @@ function Features() {
             <Image src={Sn3} alt="Sn3" className="rounded-md shadow-2xl" />
           </div>
         </div> */}
+        </div>
+      </motion.div>
+      <div className="block lg:hidden">
+        <Image src={Sn3} alt="" className="w-full lg:w-full shadow-2xl pb-12" />
       </div>
-    </motion.div>
+    </>
   );
 }
 
